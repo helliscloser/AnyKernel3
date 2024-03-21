f44e215ad5a02118fc733fb7259d8475e552b7e1
Rumais <85108733+helliscloser@users.noreply.github.com>
2024-03-21 23:27:20 +0530

    Change Kernel flashing message

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Kernel by aryannn999 @ xda-developers
+kernel.string=Flashing Kernel ...
 do.devicecheck=1
 do.modules=0
 do.systemless=1
